fdffd115c27c5d97f0786c4ffa0312548efdd679
Ogochukwu Odom <ogochukwuodom@gmail.com>
2021-05-11 00:35:52 +0100

    ch: add logout - not working

---
+++ pages/courses/grades/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="top my-3">
+      <small>Overview</small>
+      <h4>Results</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "dashboard"
+};
+</script>
+
+<style></style>
